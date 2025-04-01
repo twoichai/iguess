1a114db2cea4a1f62f333ed0afb050eab1b65f97
twoichai <ibenyuk@gmail.com>
2025-04-01 23:08:29 +0200

    removed stuff

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import { DirectMessage } from './components/DirectMessage';
 import {setupOnlineStatus} from "./components/UserStatus";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBkSO2hqzLXIJv_zK0Fl7E6yJHxlRTgIM",
-    authDomain: "iguess-2bb2b.firebaseapp.com",
-    projectId: "iguess-2bb2b",
-    storageBucket: "iguess-2bb2b.firebasestorage.app",
-    messagingSenderId: "207975405411",
-    appId: "1:207975405411:web:93c050b93b1e9725d3a556",
-    measurementId: "G-968629FZJB"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
